971280c8aa8bbbbb2a812b9cedb5a5489d89f9f2
Julian Belmartino <belmartinojulian@gmail.com>
2022-09-01 21:17:08 -0300

    entrega

--- createTable.js
+++ createTable.js
@@ -4,10 +4,9 @@ const knex = require('knex') (options)
 
 knex.schema.createTable('productos', table =>{
     table.increments('id')
-    table.string('name')
-    table.string('email')
-    table.string('password')
-    table.string('edad')
+    table.string('title')
+    table.integer('price')
+    table.string('thumbnail')
 })
 .then(() => console.log('table created'))
 .catch((err) => {console.log(err); throw err})
